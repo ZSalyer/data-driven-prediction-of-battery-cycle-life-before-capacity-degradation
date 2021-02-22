c5ebc56dec3e8518819c46ed9040dc161963eb22
Salyer Zach (FCA) <t0597zs@cag.cdroot.net>
2021-02-22 07:08:53 -0500

    Discharge Voltage Extraction
    Extract only portion of the cycle corresponding to the discharge voltage curve for future processing

--- cycle-data-plotter.py
+++ cycle-data-plotter.py
@@ -47,3 +47,69 @@ Make a plot of Voltage v. Q_100 - Q_10 for a random selection of cells.
 import random
 
 random_cell_selection = random.choice(list(test_data.keys()))
+cell_data = test_data[random_cell_selection]
+
+#Plot I,V,T for randomly selected cell at cycles 1 and 100
+def plot_IVT(cell_data, cycle_number):
+    fig, axs = plt.subplots(3, 1)
+    axs[0].title.set_text('Cycle Number: {}'.format(str(cycle_number)))
+    axs[0].plot(cell_data['cycles'][str(cycle_number)]['t'], cell_data['cycles'][str(cycle_number)]['I'])
+    axs[0].set_ylabel('Current [A]')
+    axs[1].plot(cell_data['cycles'][str(cycle_number)]['t'], cell_data['cycles'][str(cycle_number)]['V'])
+    axs[1].set_ylabel('Voltage [V]')
+    axs[2].plot(cell_data['cycles'][str(cycle_number)]['t'], cell_data['cycles'][str(cycle_number)]['T'])
+    axs[2].set_ylabel('Temp [°C]')
+    axs[2].set_xlabel('Time [s]')
+
+plot_IVT(cell_data, 1)
+plot_IVT(cell_data, 100)
+
+#%% Recreate Fig. 2 from Paper
+from tqdm import tqdm
+from scipy import interpolate
+import numpy as np
+
+def extract_discharge_voltage_info(test_data):
+    #Add dictionary key that contains info from only discharge portion of the cycle
+    for cell_id in tqdm(test_data.keys()):     #For each cell
+        cell_data = test_data[cell_id]   #Extract cell data
+        for cycle_num in range(len(cell_data['cycles'])):
+            test_data[cell_id]['cycles'][str(cycle_num)]['Qd_discharge'] = [Qd for idx, Qd in 
+                                                                     enumerate(test_data[cell_id]['cycles'][str(cycle_num)]['Qd'])
+                                                                     if test_data[cell_id]['cycles'][str(cycle_num)]['Qd'][idx] > 1e-3]
+            test_data[cell_id]['cycles'][str(cycle_num)]['V_discharge'] = [V for idx, V in 
+                                                                     enumerate(test_data[cell_id]['cycles'][str(cycle_num)]['V'])
+                                                                     if test_data[cell_id]['cycles'][str(cycle_num)]['Qd'][idx] > 1e-3]
+            
+            #Normalize data by setting length to be 400 datapoints
+            def normalize_data(new_size, data):
+                f = interpolate.interp1d(range(len(data)),data)
+                return f(np.linspace(0, new_size, new_size+1))
+            
+            try:
+                test_data[cell_id]['cycles'][str(cycle_num)]['Qd_discharge'] = normalize_data(400, test_data[cell_id]['cycles'][str(cycle_num)]['Qd_discharge'])
+                test_data[cell_id]['cycles'][str(cycle_num)]['V_discharge'] = normalize_data(400, test_data[cell_id]['cycles'][str(cycle_num)]['V_discharge'])
+            catch
+                if cycle_num != 0:
+                    print('Normalization failed for cycle number: {}'.format(str(cycle_num)))
+            
+def create_Fig2(cell_data, all_cell_data):
+    #Initialize Figure
+    fig, axs = plt.subplots(1, 3)
+    
+    #Create Fig2a (discharge capacity curves for 100th and 10th cycles for random cell)
+    axs[0].plot(cell_data['cycles']['10']['Qd_discharge'], cell_data['cycles']['10']['V_discharge'], label='Cycle 10')
+    axs[0].plot(cell_data['cycles']['100']['Qd_discharge'], cell_data['cycles']['100']['V_discharge'], label='Cycle 100')
+    axs[0].legend()
+    axs[0].set_ylabel('Voltage [V]')
+    axs[0].set_xlabel('Discharge Capacity [Ah]')
+    
+    #Create Fig2b
+    
+    #Create Fig2c
+    
+extract_discharge_voltage_info(test_data)  
+create_Fig2(cell_data, test_data)
+
+random_cell_selection = random.choice(list(test_data.keys()))
+cell_data_update = test_data[random_cell_selection]
